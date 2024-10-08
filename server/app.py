0599a463a3e6a9338bafd7e4745e317a70e980f1
Joeken10 <joekentafir@gmail.com>
2024-10-09 00:46:42 +0300

    first commit

--- server/app.py
+++ server/app.py
@@ -3,7 +3,6 @@
 from flask import Flask, jsonify, request, make_response
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
-
 from models import db, Plant
 
 app = Flask(__name__)
@@ -16,9 +15,8 @@ db.init_app(app)
 
 api = Api(app)
 
-
+# Resource to handle /plants route
 class Plants(Resource):
-
     def get(self):
         plants = [plant.to_dict() for plant in Plant.query.all()]
         return make_response(jsonify(plants), 200)
@@ -35,21 +33,51 @@ class Plants(Resource):
         db.session.add(new_plant)
         db.session.commit()
 
-        return make_response(new_plant.to_dict(), 201)
-
+        return make_response(jsonify(new_plant.to_dict()), 201)
 
 api.add_resource(Plants, '/plants')
 
-
+# Resource to handle /plants/<id> route
 class PlantByID(Resource):
-
     def get(self, id):
-        plant = Plant.query.filter_by(id=id).first().to_dict()
-        return make_response(jsonify(plant), 200)
+        plant = Plant.query.filter_by(id=id).first()
+        if plant:
+            return make_response(jsonify(plant.to_dict()), 200)
+        else:
+            return make_response(jsonify({"error": "Plant not found"}), 404)
 
+    # PATCH method to update plant information
+    def patch(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        if plant:
+            data = request.get_json()
+
+            # Update only provided fields
+            if "name" in data:
+                plant.name = data["name"]
+            if "image" in data:
+                plant.image = data["image"]
+            if "price" in data:
+                plant.price = data["price"]
+            if "is_in_stock" in data:
+                plant.is_in_stock = data["is_in_stock"]
+
+            db.session.commit()
+            return make_response(jsonify(plant.to_dict()), 200)
+        else:
+            return make_response(jsonify({"error": "Plant not found"}), 404)
+
+    # DELETE method to remove a plant
+    def delete(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        if plant:
+            db.session.delete(plant)
+            db.session.commit()
+            return make_response('', 204)  # 204 No Content on success
+        else:
+            return make_response(jsonify({"error": "Plant not found"}), 404)
 
 api.add_resource(PlantByID, '/plants/<int:id>')
-
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
